32eab9dbdbb31bcfae2415b6807d9df9b7a0cf7d
Niederhuth <niederhu@msu.edu>
2023-05-10 15:15:21 -0400

    stuff

--- scripts/pangenome/index.sh
+++ scripts/pangenome/index.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=60GB
 #SBATCH --job-name index
-#SBATCH --output=%x-%j.SLURMout
+#SBATCH --output=job_reports/%x-%j.SLURMout
 
 #Set this variable to the path to wherever you have conda installed
 conda="${HOME}/miniconda3"
